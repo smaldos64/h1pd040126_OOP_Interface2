917f2620d8487a6bbc2ee8335325a4ed809cd3e3
smaldos64 <gudumholm34@gmail.com>
2026-05-08 10:51:04 +0200

    Ændret i kommentarer i Program.cs filen, så disse er blevet mere sigende.

--- Program.cs
+++ Program.cs
@@ -10,21 +10,25 @@ namespace Interface2
             MobilePayProcessor mobilePayProcessor_Object = new MobilePayProcessor();
             EMailService eMailService_Object = new EMailService();
 
+            /* Linjen herunder gør brug af den første Constructor i CheckoutManager, og den overholder 
+             * derfor alle principperne i SOLID.
+             * Vi sender en klasse der implementerer IPaymentProcessor (CreditCardProcessor) og
+             * ingen klasse der implementerer INotifier, da denne parameter er optional. */
             CheckoutManager checkoutManager_Object = new CheckoutManager(creditCardProcessor_Object);
+
+            /* Linjen herunder gør brug af den anden Constructor i CheckoutManager, 
+             * og den overholder derfor ikke alle principperne i SOLID.
+             * Vi sender en klasse der implementerer IAdvancedPaymentProvider (MobilePayProcessor), 
+             * som både implementerer IPaymentProcessor og INotifier. */
             CheckoutManager checkoutManager_Object1 = new CheckoutManager(mobilePayProcessor_Object);
 
-            // Metoden herundeer er den metode, vi bør bruge, da den udnytter det faktum,
-            // at MobilePayProcessor implementerer både IPaymentProcessor og INotifier.
-            // Hermed overholder vi også Liskov Substitution Principle (L i SOLID), da vi kan erstatte IPaymentProcessor med
-            // IAdvancedPaymentProvider uden at ændre på CheckoutManager's funktionalitet.
-            // Desuden overholder vi S i SOLID (Single Responsibility Principle), da CheckoutManager
-            // nu får 2 forskellige Interfaces, implementeret i 2 klasser,
-            // der kun har én funktionalitet.
-            // Interface segregation principle (I i SOLID) overholdes også,
-            // da vi har opdelt funktionaliteten i flere interfaces,
+            /* Linjen herunder gør brug af den første Constructor i CheckoutManager, 
+             * og den overholder derfor alle principperne i SOLID.
+             * Vi sender en klasse der implementerer IPaymentProcessor (MobilePayProcessor) 
+             * og en klasse der implementerer INotifier (EMailService). */
             CheckoutManager checkoutManager_Object2 = new CheckoutManager(mobilePayProcessor_Object, eMailService_Object);
 
-            // Linjen herunder giver compilr fejl, da CheckoutManager's constructor kræver en IPaymentProcessor,
+            // Linjen herunder giver compiler fejl, da CheckoutManager's constructor kræver en IPaymentProcessor,
             // og EMailService implementerer ikke dette interface.
             //CheckoutManager checkoutManager_Object3 = new CheckoutManager(eMailService_Object);
 
